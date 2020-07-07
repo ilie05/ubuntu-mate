27e81832a353c2c58416c9e25fa7be3d268cb4ee
Ilie Petrasco <Ilie.Petrasco@endava.com>
2020-07-07 21:08:41 +0300

    send data on button click

--- static/main.js
+++ static/main.js
@@ -1,19 +1,43 @@
-const sendData = () => {
+const getField = (id) => document.getElementById(id);
+
+const status = getField('status');
+const position = getField('position');
+const battery = getField('battery');
+
+const sendPosition = (context) => {
+    context.disabled = true;
+    status.value = 'Command Sent...';
     fetch('/send')
         .then(res => {
             if (res.ok && res.status === 200)
                 return res.json();
         })
-        .then(json => {
-            console.log(json);
+        .then(data => {
+            console.log(data);
+            status.value = 'Moving...';
+            position.value = '';
             return fetch('/move')
         })
         .then(res => {
             if (res.ok && res.status === 200)
                 return res.json();
         })
-        .then(json => {
-            console.log(json);
+        .then(data => {
+            status.value = 'Position Updated...';
+            position.value = data;
+            console.log(data);
+
         })
-        .catch(err => console.log(err));
+        .catch(err => console.log(err))
+        .finally(() => {
+            context.disabled = false;
+        })
+}
+
+const getPosition = (context) => {
+
+}
+
+const getBatteryVoltage = (context) => {
+
 }
